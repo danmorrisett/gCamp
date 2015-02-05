7c9aa6870b0f61af5fde7acd57340f26fdc17c7c
Danmorrisett <danmorrisett@gMAIL.com>
2015-02-05 15:27:54 -0700

    Final push

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,11 @@
 class WelcomeController < ApplicationController
 
   def index
+    @quotesArray = [
+      {author: '-Cayla Hayes', quote: "gCamp has changed my life! It's the best tool I've ever used."},
+      {author: '-Leta Jaskolski', quote: "Before gCamp I was a disorderly slob. Now I'm more organized than I've ever been"},
+      {author: '-Lavern Upton', quote: "Don't hesitate - sign up right now! You'll never be the same."}
+      ]
+
   end
 end
