ce53cda6ca682e1bb717b3f9cfafc841160abf8d
Danmorrisett <danmorrisett@gMAIL.com>
2015-02-10 14:36:21 -0700

    User can see links

--- app/controllers/common_questions_controller.rb
+++ app/controllers/common_questions_controller.rb
@@ -7,18 +7,15 @@ class CommonQuestionsController < ApplicationController
       gCamp may eventually replace all need for paper and pens in the entire world.
       Well, maybe not, but it's going to be pretty cool.", "what-is-gcamp"),
 
-      CommonQuestions.new("how-do I join gCamp?",
+      CommonQuestions.new("How do I join gCamp?",
 
         "As soon as it's ready for the public, you'll see a signup link in the upper right.
-        Once that's there, just click it and fill in the form!",
+        Once that's there, just click it and fill in the form!", "how-do-i-join-gcamp"),
 
-        "When will gCamp be finished?"),
-
-        CommonQuestions.new("gCamp is a work in progress.",
-        "That being said, it should be fully functional in the next few weeks.
-        Functional. Check in daily for new features and awesome functionality.",
-
-        "It's going to blow your mind. Organization is just a click away. Amazing!"),
+        CommonQuestions.new("When will gCamp be finished?",
+        "gCamp is a work in progress. That being said, it should be fully functional in the next few weeks.
+        Functional. Check in daily for new features and awesome functionality.
+        It's going to blow your mind. Organization is just a click away. Amazing!", "when-will-gcamp-be-finished?"),
     ]
   end
 end
--- app/models/common_questions.rb
+++ app/models/common_questions.rb
@@ -1,8 +1,21 @@
 class CommonQuestions
   attr_reader :question, :answer, :slug
+
   def initialize(question, answer, slug)
-    @question = question,
-    @answer = answer,
+    @question = question
+    @answer = answer
     @slug = slug
+  end
+
+  def question
+    @question
+  end
+
+  def answer
+    @answer
+  end
+  
+  def slug
+    @slug
   end
 end
