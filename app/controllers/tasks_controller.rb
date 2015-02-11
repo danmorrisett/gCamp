748fff4bfd1e283b344c8d91b946a9031acc8c48
Danmorrisett <danmorrisett@gMAIL.com>
2015-02-10 22:41:17 -0700

    Added Task page

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  def index
+    @task =Task.all
+  end
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(tasks_params)
+      if @task.save
+        redirect_to task_path(@task), notice: 'Task was successfully created.'
+      else
+        render :new
+      end
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(tasks_params)
+      redirect_to task_path, notice: 'Task was successfully updated'
+    else
+      render :edit
+    end
+  end
+
+  def destory
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+    private
+
+    def tasks_params
+      params.require(:task).permit(:description)
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
-get 'terms', to: 'terms#index'
-get 'about', to: 'about#index'
-get 'faq', to: 'common_questions#index'
+  get 'terms', to: 'terms#index'
+  get 'about', to: 'about#index'
+  get 'faq', to: 'common_questions#index'
 
+
+resources :tasks
 end
