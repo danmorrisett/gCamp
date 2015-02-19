a922f09fdd9b395eb7838b0a097cdf8f3068399e
Danmorrisett <danmorrisett@gMAIL.com>
2015-02-19 16:32:22 -0700

    Added user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,27 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_path
+      flash[:notice] = 'Task was successfully created.'
+      redirect_to users_path(@user)
     else
       render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path
+    else
+      render :edit
     end
   end
 
