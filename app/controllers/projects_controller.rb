e61b0467ef3158a0669d0a400f1c538c3518e371
Danmorrisett <danmorrisett@gMAIL.com>
2015-02-20 16:31:00 -0700

    Added Projects page

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,54 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      flash[:notice] = 'Project was successfully created.'
+      redirect_to projects_path(@project)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @project = Project.find(params[:id])
+  end
+
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      flash[:notice] = 'Project was successfully updated'
+      redirect_to project_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path, notice: "Project has been successfully deleted"
+  end
+
+
+
+
+
+
+  private
+  def project_params
+    params.require(:project).permit(:first_name, :last_name, :email)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
 
   resources :tasks
   resources :users
+  resources :projects
 end
