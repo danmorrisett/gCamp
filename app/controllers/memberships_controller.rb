d444c0ad01f885595a7ea0c1b7ad6f0272fbb54a
Danmorrisett <danmorrisett@gMAIL.com>
2015-03-16 17:12:29 -0600

    Memberships

---
+++ app/controllers/memberships_controller.rb
@@ -0,0 +1,48 @@
+class MembershipsController < ApplicationController
+
+  before_action do
+    @project = Project.find(params[:project_id])
+  end
+
+  def index
+    @membership = @project.memberships.new
+  end
+
+  def create
+    @membership = @project.memberships.new(membership_params)
+    if @membership.save
+      flash[:success] = "#{@membership.user.full_name} was successfully added"
+      redirect_to project_memberships_path
+    else
+      render :index
+    end
+  end
+
+  def edit
+    @membership = Membership.find(params[:id])
+  end
+
+  def update
+    @membership = Membership.find(params[:id])
+    if @membership.update(membership_params)
+      flash[:success] = "#{@membership.user.full_name} was successfully updated"
+      redirect_to project_memberships_path
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @membership = Membership.find(params[:id])
+    @membership.destroy
+    flash[:success] = "#{@membership.user.full_name} was removed from project"
+    redirect_to project_memberships_path
+  end
+
+  private
+
+  def membership_params
+    params.require(:membership).permit(:role, :project_id, :user_id)
+  end
+
+end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,4 +3,6 @@ class Membership < ActiveRecord::Base
   belongs_to :user
   belongs_to :project
 
+  ROLE = ['Member', 'Owner']
+
 end
