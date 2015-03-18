e2c820bec75ce30f55b33cb13a65d49157956cf4
Danmorrisett <danmorrisett@gMAIL.com>
2015-03-18 15:36:58 -0600

    Added validations for membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,5 +1,8 @@
 class Membership < ActiveRecord::Base
 
+  validates :user, presence: true
+  validates :user, uniqueness: {scope: :project_id, message: "has already been added to this project" }
+
   belongs_to :user
   belongs_to :project
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true
   def full_name
     "#{self.first_name} #{self.last_name}"
   end
+
+  has_many :memberships
+
 end
