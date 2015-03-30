1a982be78d370e46cc433a44b883d5270621e05c
Danmorrisett <danmorrisett@gMAIL.com>
2015-03-30 14:27:14 -0600

    Users can only see projects they belong to

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,10 +1,12 @@
 class ProjectsController < ApplicationController
 
   before_action :ensure_current_user
-
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_membership, only: [:show, :edit, :update, :destroy]
 
   def index
-    @projects = Project.all
+    @projects = current_user.projects
+
   end
 
   def new
@@ -23,15 +25,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = 'Project was successfully updated'
       redirect_to project_path
@@ -41,12 +40,23 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     @project.destroy
     redirect_to projects_path, notice: "Project was successfully deleted"
   end
 
   private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  def ensure_membership
+    if !current_user.memberships.find_by(:project_id => @project.id)
+      flash[:error] = 'You do not have access to that project'
+      redirect_to projects_path
+    end
+  end
+
   def project_params
     params.require(:project).permit(:name)
   end
