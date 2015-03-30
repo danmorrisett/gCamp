2cb606ae456a880f99157c5037b2d00724f07486
Danmorrisett <danmorrisett@gMAIL.com>
2015-03-29 19:37:15 -0600

    Updated Rake tasks

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -6,7 +6,9 @@ feature "Check users pages w flash and validations" do
   end
 
   scenario "Users can see index page" do
+
     visit users_path
+
     expect(page).to have_content ("Users")
     expect(page).to have_content ("New User")
     expect(page).to have_content ("Name")
@@ -14,33 +16,46 @@ feature "Check users pages w flash and validations" do
   end
 
   scenario "Users can create new user" do
+
     visit users_path
+
     click_on("New User")
+
     expect(page).to have_content ("New User")
+
     fill_in "user_first_name", with: nil
     fill_in "user_last_name", with: nil
     fill_in "user_email", with: nil
     fill_in "user_password", with: nil
+
     click_on ("Create User")
+
     expect(page).to have_content ("1 error prohibited this form from being saved")
     expect(page).to have_content ("First name can't be blank")
     expect(page).to have_content ("Last name can't be blank")
     expect(page).to have_content ("Email can't be blank")
     expect(page).to have_content ("Password can't be blank")
+
     fill_in "user_first_name", with: "Bill"
     fill_in "user_last_name", with: "Clinton"
     fill_in "user_email", with: "email@fake.com"
     fill_in "user_password", with: "password"
     fill_in "user_password_confirmation", with: "password"
+
     click_on ("Create User")
+
     expect(page).to have_content ("User was successfully created.")
   end
 
   scenario "users can see specific user details" do
+
     new_user = User.new({first_name: "Freddie", last_name: "Mercury", email: "cool@names.com", password: "password", password_confirmation: "password"})
     new_user.save
+
     visit users_path
+
     click_on ("Freddie Mercury")
+
     expect(page).to have_content ("Freddie")
     expect(page).to have_content ("Mercury")
     expect(page).to have_content ("cool@names.com")
@@ -49,33 +64,49 @@ feature "Check users pages w flash and validations" do
   end
 
   scenario "users can edit users detials except password" do
+
     new_user = User.new({first_name: "Bill", last_name: "Clinton", email: "cool@names.com", password: "password", password_confirmation: "password"})
     new_user.save
+
     visit "/users/#{new_user[:id]}"
+
     click_on ("Edit")
+
     expect(page).to have_content ("Edit User")
+
     fill_in "user_first_name", with: nil
     fill_in "user_last_name", with: nil
     fill_in "user_email", with: nil
+
     click_on ("Update User")
+
     expect(page).to have_content ("1 error prohibited this form from being saved")
     expect(page).to have_content ("First name can't be blank")
     expect(page).to have_content ("Last name can't be blank")
     expect(page).to have_content ("Email can't be blank")
+
     fill_in "user_first_name", with: "Bill"
     fill_in "user_last_name", with: "Clinton "
     fill_in "user_email", with: "email@fake.com"
+
     click_on ("Update User")
+
     expect(page).to have_content ("User was successfully updated.")
+
     expect(current_path).to eq("/users")
   end
 
   scenario "users can delete user" do
+
     new_user = User.new({first_name: "Freddie", last_name: "Mercury", email: "cool@names.com", password: "password"})
     new_user.save
+
     visit "/users/#{new_user[:id]}"
+
     click_on ("Edit")
+
     click_on ("Delete User")
+    
     expect(page).to have_content ("User was successfully deleted.")
     expect(page).to have_no_content ("Freddie Mercury")
     expect(current_path).to eq("/users")
