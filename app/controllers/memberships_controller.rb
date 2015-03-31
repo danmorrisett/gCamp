b8cd94a2bfda08f5e2db39129e6829c31780f5e2
Danmorrisett <danmorrisett@gMAIL.com>
2015-03-31 11:00:38 -0600

    Users can delete their own memberships to any project

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,8 +1,9 @@
 class MembershipsController < ApplicationController
 
   before_action :set_project
+  before_action :ensure_membership, only: [:index]
   before_action :ensure_current_user
-  before_action :ensure_membership
+  before_action :ensure_current_user_member, only: [:destroy]
 
   before_action do
     @project = Project.find(params[:project_id])
@@ -16,7 +17,7 @@ class MembershipsController < ApplicationController
     @membership = @project.memberships.new(membership_params.merge(role: params[:membership][:role].to_i))
     if @membership.save
       flash[:notice] = "#{@membership.user.full_name} was successfully added"
-      redirect_to project_memberships_path
+      redirect_to project_memberships_path(@project)
     else
       render :index
     end
@@ -40,8 +41,9 @@ class MembershipsController < ApplicationController
     @membership = Membership.find(params[:id])
     @membership.destroy
     flash[:notice] = "#{@membership.user.full_name} was removed from project"
-    redirect_to project_memberships_path
+    redirect_to projects_path
   end
+
 
   private
 
@@ -58,6 +60,13 @@ class MembershipsController < ApplicationController
 
   def membership_params
     params.require(:membership).permit(:role, :project_id, :user_id)
+  end
+
+  def ensure_current_user_member
+    unless current_user.id == @project.memberships.where(user_id: current_user.id)
+      flash[:error] = "You do not have access"
+      redirect_to projects_path
+    end
   end
 
 end
